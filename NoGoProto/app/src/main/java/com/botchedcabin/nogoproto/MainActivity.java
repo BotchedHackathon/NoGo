653b1fb7265de9b28960a0d84af4b4db190b6175
Mark Flores <markflores@MFF-mbp13.local>
2015-01-31 12:27:38 -0800

    New Game Button action

--- NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/MainActivity.java
+++ NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button newButton = (Button) findViewById(R.id.newbutton);
+        newButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                newButton.setText("Game Started");
+            }
+        });
     }
 
 
