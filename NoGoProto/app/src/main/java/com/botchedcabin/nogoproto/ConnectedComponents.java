d58240001ceedaa9ddd218fb35f26eb385b01e28
Michael Nip <mdnip@engineering.ucsb.edu>
2015-01-31 14:53:59 -0800

    Started connected component tracker

--- NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/ConnectedComponents.java
+++ NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/ConnectedComponents.java
@@ -1,10 +1,11 @@
 package com.botchedcabin.nogoproto;
 
 /**
+ * Connected component data structure for NoGo.
  * Created by Michael Nip on 1/31/2015.
  */
 public class ConnectedComponents {
 
-    public Coordinate[][] ComponentReferences = new Coordinate[1][1];
+    public Coordinate[][] ComponentReferences = new Coordinate;
 
 }
--- NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/Coordinate.java
+++ NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/Coordinate.java
@@ -1,27 +1,42 @@
 package com.botchedcabin.nogoproto;
 
 /**
+ * Storage for references to coordinates of node one level up.
+ * [x,y] = [-1,-1] indicates the node has no parents.
+ * x is row coordinate and y is column coordinate
+ *
  * Created by Michael Nip on 1/31/2015.
  */
+
 public class Coordinate {
     // Stores a tuple of coordinate values.
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     static final int defaultCoord = -1;
 
-    /*
-    * Constructor Methods
+    /**
+    * Default constructor.  Sets node to top of tree.
+    */
+    public Coordinate(){ setCoordinate(defaultCoord,defaultCoord); }
+
+    /**
+     * More elaborate constructor.  Sets parent node with coordinate [x1,y1]
      */
     public Coordinate(int x1, int y1){ setCoordinate(x1,y1); }
 
-    public Coordinate(){ setCoordinate(defaultCoord,defaultCoord); }
 
+    /**
+     * Set parent to [x1,y1]
+     */
     public void setCoordinate(int x1, int y1){
         x = x1;
         y = y1;
     }
 
+    /**
+     * Fetch x or y coordinate of parent node
+     */
     public int getXCoordinate(){
         return x;
     }
