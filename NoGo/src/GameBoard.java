8a5ea0ec77dd9912c4ccb29b13988cc58d0bd831
Sean Chong <banannigans@gmail.com>
2015-01-31 12:46:08 -0800

    changed method 'checkMove' to 'validateMove' added method 'checkGameState'

--- NoGo/src/GameBoard.java
+++ NoGo/src/GameBoard.java
@@ -1,19 +1,19 @@
 
 public class GameBoard {
-	
-	public int m_boardSize;
-	public int[][] boardState = new int[m_boardSize + 2][m_boardSize + 2];
-	public int blackScore;
-	public int whiteScore;
-	static final int defaultGameBoardSize = 9;
-	
+ 
+ public int m_boardSize;
+ public int[][] boardState = new int[m_boardSize + 2][m_boardSize + 2];
+ public int blackScore;
+ public int whiteScore;
+ static final int defaultGameBoardSize = 9;
+ 
     /**
      * Default constructor.
      * Sets board size to 9x9
      */
-	public GameBoard(){
-		initializeGameBoard(defaultGameBoardSize);
-	}
+ public GameBoard(){
+  initializeGameBoard(defaultGameBoardSize);
+ }
     
     /**
      * A constructor.
@@ -28,38 +28,51 @@ public class GameBoard {
      * Initialize the board
      * Sets the board size and blacks and white score
      */
-	private void initializeGameBoard(int boardSize){
-		m_boardSize = boardSize;
-		blackScore = 0;
-		whiteScore = 0;
-		
-		// Specify boundary nodes
-		
-	}
-	
-	
-	
-	public boolean checkMove(int x, int y){
-		// Check if move is legal
-		return true;
-	}
-	
-	public void placeBlack(int x, int y){
-		// Place a black stone (set to 1)
-		boardState[x][y]=1;
-	}
-	
-	public void placeWhite(int x, int y){
-		// Place a white stone (set to 2)
-		boardState[x][y]=2;
-	}
-	
-	public void calculateScore(){
-		// Update Score
-		blackScore = 0;
-		whiteScore = 0;
-	}
-	
+ private void initializeGameBoard(int boardSize){
+  m_boardSize = boardSize;
+  blackScore = 0;
+  whiteScore = 0;
+  
+  // Specify boundary nodes
+  
+ }
+ 
+ /**
+  * Method to convert a gameboard coordinate to it's proper index value
+  */
+ private int convertCoordToIndex(int x, int y){
+ 
+ 
+ }
+ 
+ public boolean validateMove(int x, int y){
+  // Check if move is legal
+  return true;
+ }
+ 
+ /**
+  * 
+  */
+ public void checkGameState(int x, int y){
+     
+ }
+ 
+ public void placeBlack(int x, int y){
+  // Place a black stone (set to 1)
+  boardState[x][y]=1;
+ }
+ 
+ public void placeWhite(int x, int y){
+  // Place a white stone (set to 2)
+  boardState[x][y]=2;
+ }
+ 
+ public void calculateScore(){
+  // Update Score
+  blackScore = 0;
+  whiteScore = 0;
+ }
+ 
 
 
 }
