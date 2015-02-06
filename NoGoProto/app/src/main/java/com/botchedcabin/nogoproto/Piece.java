6ed6741181cc50779a9ed488ee961453cbc166b9
Michael Nip <mdnip@engineering.ucsb.edu>
2015-02-06 11:54:18 -0800

    Made some changes to Piece class.  Added GoPiece class as subclass of Piece

--- NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/Piece.java
+++ NoGoProto/app/src/main/java/com/botchedcabin/nogoproto/Piece.java
@@ -1,8 +1,23 @@
 package com.botchedcabin.nogoproto;
 
-/**
+/** Generic Game Piece Class
  * Created by Michael Nip on 2/5/2015.
  */
-public enum Piece {
-    Black, White, Empty, Border
+public class Piece {
+    protected Color pieceColor;
+
+    /**
+     * Constructor for Piece Object
+     */
+    Piece(){
+    }
+
+    Piece(Color pieceColor){
+        this.pieceColor = pieceColor;
+    }
+
+    public Color getColor(){
+        return pieceColor;
+    }
+
 }
